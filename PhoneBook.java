e8b16336b67bda61319a891b8583ebd3859452fd
Vitaliy <60990529+VitaliyKrasnov@users.noreply.github.com>
2024-06-04 19:11:11 +0600

    Update PhoneBook.java

--- PhoneBook.java
+++ PhoneBook.java
@@ -10,21 +10,19 @@ class HeapSort {
      * @param heapSize количество элементов в куче (длина массива)
      * @param index    индекс элемента-родителя
      */
-    public static void heapify(Object[] heap, int heapSize, int index) {
+    public static void heapify(Map.Entry<String, List<String>>[] heap, int heapSize, int index) {
         int left = 2 * index + 1;
         int right = 2 * index + 2;
         int largest = index;
 
-        if (left <= (heapSize - 1)
-                && ((Map.Entry<String, List<String>>) heap[left]).getValue().size() < ((Map.Entry<String, List<String>>) heap[largest]).getValue().size()) {
+        if (left <= (heapSize - 1) &&  heap[left].getValue().size() < heap[largest].getValue().size()) {
             largest = left;
         }
-        if (right <= (heapSize - 1)
-                && ((Map.Entry<String, List<String>>) heap[right]).getValue().size() < ((Map.Entry<String, List<String>>) heap[largest]).getValue().size()) {
+        if (right <= (heapSize - 1) && heap[right].getValue().size() < heap[largest].getValue().size()) {
             largest = right;
         }
         if (largest != index) {
-            Map.Entry<String, List<String>> tmp = (Map.Entry<String, List<String>>) heap[largest];
+            Map.Entry<String, List<String>> tmp = heap[largest];
             heap[largest] = heap[index];
             heap[index] = tmp;
             heapify(heap, heapSize, largest);
@@ -35,7 +33,7 @@ class HeapSort {
      * @param tree     куча (массив элементов) которую превращаем в дерево
      * @param heapSize количество элементов в куче (размер массива)
      */
-    public static void buildTree(Object[] tree, int heapSize) {
+    public static void buildTree(Map.Entry<String, List<String>>[] tree, int heapSize) {
         int lastParentIndex = heapSize / 2 - 1;
         for (int i = lastParentIndex; i >= 0; i--) {
             heapify(tree, heapSize, i);
@@ -46,11 +44,11 @@ class HeapSort {
      * @param sortArray массив элементов который нужно отсрортировать
      * @param heapSize  размер массива
      */
-    public static void heapSort(Object[] sortArray, int heapSize) {
+    public static void heapSort(Map.Entry<String, List<String>>[] sortArray, int heapSize) {
         // Введите свое решение ниже
         buildTree(sortArray, heapSize);
         for (int i = (heapSize - 1); i > 0; i--) {
-            Map.Entry<String, List<String>> tmp = (Map.Entry<String, List<String>>) sortArray[0];
+            Map.Entry<String, List<String>> tmp = sortArray[0];
             sortArray[0] = sortArray[i];
             sortArray[i] = tmp;
             heapify(sortArray, i, 0);
@@ -63,11 +61,10 @@ public class PhoneBook {
     static Map<String, List<String>> phoneBook = new HashMap<>();
 
     public static void printPhoneBook() {
-        Object[] phones = phoneBook.entrySet().toArray();
+        Map.Entry<String, List<String>>[] phones = phoneBook.entrySet().toArray(new Map.Entry[0]);
         HeapSort.heapSort(phones, phones.length);
-        for (Object phone : phones) {
-            Map.Entry<String, List<String>> item = (Map.Entry<String, List<String>>) phone;
-            System.out.println(String.format("%1$-8s: %2$s", item.getKey(), item.getValue().toString()));
+        for (Map.Entry<String, List<String>> phone : phones) {
+            System.out.println(String.format("%1$-8s: %2$s", phone.getKey(), phone.getValue().toString()));
         }
     }
 
